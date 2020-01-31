a4823ad71351973001d2df768ad82845af8c7cc5
vladimir <vladim.volovenko@gmail.com>
2020-02-01 00:10:36 +0200

    ltsson 23 props in dialog

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,22 +1,25 @@
 import React from "react";
 import s from "./dialogs.module.css";
+import { NavLink } from "react-router-dom";
+import DialogItem from "./dialog/dialogItem";
+import Message from "./mesage/message";
 
 const Dialogs = props => {
   return (
     <div className={s.dialogs}>
       <div className={s.dialogs_items}>
-        <div className={s.dialog}>Dimych</div>
-        <div className={s.dialog + ' ' + s.active}>Andrey</div>
-        <div className={s.dialog}>Sveta</div>
-        <div className={s.dialog}>Sasha</div>
-        <div className={s.dialog}>Victor</div>
-        <div className={s.dialog}>Valera</div>
+        <DialogItem name="Dimych" id="1" />
+        <DialogItem name="Andrey" id="2" />
+        <DialogItem name="Sveta" id="3" />
+        <DialogItem name="Sasha" id="4" />
+        <DialogItem name="Victor" id="5" />
+        <DialogItem name="Valera" id="6" />
       </div>
 
       <div className={s.messages_items}>
-        <div className={s.message}>Hi!</div>
-        <div className={s.message}>Hoe are your it-kamasutra?</div>
-        <div className={s.message}>You!</div>
+        <Message message='Hi!'/>
+        <Message message='How are your it-kamasutra?!'/>
+        <Message message='You!'/>
       </div>
     </div>
   );
---
+++ src/components/Dialogs/mesage/message.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+// import s from './dialogItem.module.css';
+
+
+const Message = (props) => {
+
+  return (
+    <div>{props.message}</div>
+  );
+};
+
+export default Message;
