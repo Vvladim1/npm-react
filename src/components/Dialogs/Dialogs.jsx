19aacff1ca49e4825c93879b59ae7f530eadec2a
vladimir <vladim.volovenko@gmail.com>
2020-02-08 23:48:52 +0200

    create 2 reducers for 2 components and delete try function

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,47 +2,46 @@ import React from "react";
 import s from "./dialogs.module.css";
 import DialogItem from "./dialog/dialogItem";
 import Message from "./mesage/message";
-import {updateNewMessageTextCreator, sendMessageCreator} from '../redux/dialogs-reducer';
+import {
+  updateNewMessageTextCreator,
+  sendMessageCreator
+} from "../redux/dialogs-reducer";
 
 const Dialogs = props => {
-  debugger;
   let dialogsElement = props.state.dialogs.map(data => (
     <DialogItem name={data.name} id={data.id} />
   ));
   let messagesElements = props.state.messages.map(datas => (
     <Message message={datas.message} />
   ));
-let sendMessage = () => {
-  alert('all right!');
-};
-let onMBodyChange = () => {};
+
+  let onMBodyChange = () => {};
 
   let newMessageBody = props.state.newMessageBody;
   let onSendMessageClick = () => {
     props.dispatch(sendMessageCreator());
-  }
+  };
 
-  let onNewMessageChange = (e) => {
-    
+  let onNewMessageChange = e => {
     let body = e.target.value;
     props.dispatch(updateNewMessageTextCreator(body));
-  }
+  };
 
   return (
     <div className={s.dialogs}>
       <div className={s.dialogs_items}>{dialogsElement}</div>
       <div>
-      <div className={s.messages_items}>{messagesElements}</div>
-      <div>
+        <div className={s.messages_items}>{messagesElements}</div>
         <div>
-          <textarea  onChange={onNewMessageChange}
-                    value={newMessageBody}
-                    placeholder='send yor message'
-                    
-                    />
+          <div>
+            <textarea
+              onChange={onNewMessageChange}
+              value={newMessageBody}
+              placeholder="send yor message"
+            />
+          </div>
+          <button onClick={onSendMessageClick}>add message</button>
         </div>
-        <button onClick={onSendMessageClick}>add message</button>
-      </div>
       </div>
     </div>
   );
